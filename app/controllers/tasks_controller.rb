f542146be4234e9bb8b6b971e9e74fc2f42fa7a0
fahmi <seichan83@gmail.com>
2017-10-18 16:26:32 +0800

    Added Task controller, views and actions

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  def index
+    @list = List.find(params[:list_id])
+    @tasks = @list.tasks.all
+    @task = @list.tasks.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.new(get_params)
+    @tasks = @list.tasks.all
+    if @task.save
+      redirect_to list_tasks_path
+    else
+      render :index
+    end
+  end
+
+  def edit
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+  end
+
+  def update
+    @task = List.find(params[:list_id]).tasks.find(params[:id])
+    if @task.update(get_params)
+      redirect_to list_tasks_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = List.find(params[:list_id]).tasks.find(params[:id])
+    @task.destroy
+    redirect_to list_tasks_path
+  end
+
+  def show
+  end
+
+  private
+
+  def get_params
+    params.require(:task).permit(:title, :details, :due_date)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/edit'
+
+  get 'tasks/show'
+
   get 'lists/new'
 
   get 'lists/create'
