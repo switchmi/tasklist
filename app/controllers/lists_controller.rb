cf175e4751c18208651720deb497534043fcefcb
fahmi <seichan83@gmail.com>
2017-10-18 12:53:21 +0800

    List CRUD minus show

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,23 +1,50 @@
 class ListsController < ApplicationController
-  def new
-    
+
+
+  def index
+    @list = List.new
+    @lists = List.all
   end
 
   def create
-  end
-
-  def update
+    @lists = List.all
+    @list = List.new(get_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :index
+    end
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(get_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
   end
 
-  def index
+  def new
   end
 
   def show
   end
+
+  private
+
+  def get_params
+    params.require(:list).permit(:name)
+  end
+
 end
