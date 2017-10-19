9bd6675625dc8616b9b8cf8dd9bde71b4c7f1853
Joelle5 <32255806+Joelle5@users.noreply.github.com>
2017-10-19 11:18:36 +0800

    list_mod w priority (#1)

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,23 +1,53 @@
 class ListsController < ApplicationController
-  def new
-    
-  end
 
-  def create
-  end
-
-  def update
+  def index
+    @lists = List.all
+    @list = List.new
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      flash[:notice] = "Tasklist added !"
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if
+      @list.update_attributes(list_params)
+      redirect_to lists_path
+    else
+      render :index
+    end
   end
 
   def destroy
-  end
-
-  def index
+    @list = List.find(params[:id])
+    @list.destroy
+    flash[:alert] = "TaskList Deleted !"
+    redirect_to lists_path
   end
 
   def show
+  end
+
+
+
+private
+
+  def prepare_list
+    @list = List.find(params[:list_id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-
-  get 'lists/create'
-
-  get 'lists/update'
-
-  get 'lists/edit'
-
-  get 'lists/destroy'
-
-  get 'lists/index'
-
-  get 'lists/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'lists#index'
-  resources :lists do
+  root "lists#index"
+  shallow do
+    resources :lists do
     resources :tasks
+    end
   end
 end
